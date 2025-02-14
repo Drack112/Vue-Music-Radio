0a8adb2bc7d8a73e7b5ea5c4ffc9fc3e11f87108
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-02-14 13:14:19 -0300

    refactor: simplify main process initialization and enhance Vite configuration

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,23 +1,21 @@
 import { resolve } from "path";
-import { defineConfig, externalizeDepsPlugin, loadEnv } from "electron-vite";
 import { MainEnv } from "./env";
-
+import { defineConfig, externalizeDepsPlugin, loadEnv } from "electron-vite";
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import viteCompression from "vite-plugin-compression";
 import wasm from "vite-plugin-wasm";
-import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 
 export default defineConfig(({ command, mode }) => {
   const getEnv = (name: keyof MainEnv): string => {
     return loadEnv(mode, process.cwd())[name];
   };
-
   console.log(command);
 
-  const webPort = Number(getEnv("VITE_WEB_PORT"));
-  const serverPort = Number(getEnv("VITE_SERVER_PORT"));
+  const webPort: number = Number(getEnv("VITE_WEB_PORT") || 14558);
+  const servePort: number = Number(getEnv("VITE_SERVER_PORT") || 25884);
 
   return {
     main: {
@@ -33,6 +31,7 @@ export default defineConfig(({ command, mode }) => {
         },
       },
     },
+
     preload: {
       plugins: [externalizeDepsPlugin()],
       build: {
@@ -43,27 +42,77 @@ export default defineConfig(({ command, mode }) => {
         },
       },
     },
-    plugins: [
-      vue(),
-      AutoImport({
-        imports: [
-          "vue",
-          "vue-router",
-          "@vueuse/core",
-          {
-            "naive-ui": ["useDialog", "useMessage", "useNotification", "useLoadingBar"],
+
+    renderer: {
+      root: ".",
+      plugins: [
+        vue(),
+        AutoImport({
+          imports: [
+            "vue",
+            "vue-router",
+            "@vueuse/core",
+            {
+              "naive-ui": ["useDialog", "useMessage", "useNotification", "useLoadingBar"],
+            },
+          ],
+          eslintrc: {
+            enabled: true,
+            filepath: "./auto-eslint.mjs",
           },
-        ],
-        eslintrc: {
-          enabled: true,
-          filepath: "./auto-eslint.mjs",
+        }),
+        Components({
+          resolvers: [NaiveUiResolver()],
+        }),
+        viteCompression(),
+        wasm(),
+      ],
+      resolve: {
+        alias: {
+          "@": resolve(__dirname, "src/"),
         },
-      }),
-      Components({
-        resolvers: [NaiveUiResolver()],
-      }),
-      viteCompression(),
-      wasm(),
-    ],
+      },
+      css: {
+        preprocessorOptions: {
+          scss: {
+            silenceDeprecations: ["legacy-js-api"],
+          },
+        },
+      },
+      server: {
+        port: webPort,
+
+        proxy: {
+          "/api": {
+            target: `http://127.0.0.1:${servePort}`,
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, "/api/"),
+          },
+        },
+      },
+      preview: {
+        port: webPort,
+      },
+      build: {
+        minify: "terser",
+        publicDir: resolve(__dirname, "public"),
+        rollupOptions: {
+          input: {
+            index: resolve(__dirname, "index.html"),
+          },
+          output: {
+            manualChunks: {
+              stores: ["src/stores/data.ts", "src/stores/index.ts"],
+            },
+          },
+        },
+        terserOptions: {
+          compress: {
+            pure_funcs: ["console.log"],
+          },
+        },
+        sourcemap: false,
+      },
+    },
   };
 });
--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,5 +1,4 @@
 import { app, shell, BrowserWindow, BrowserWindowConstructorOptions } from "electron";
-import * as path from "path";
 
 class MainProcess {
   mainWindow: BrowserWindow | null = null;
@@ -11,27 +10,10 @@ class MainProcess {
       console.log("Num da");
       app.quit();
       process.exit(0);
-    } else {
-      app.on("ready", async () => {
-        console.info("🚀 Application Process Startup");
-        this.createWindow();
-      });
-    }
-  }
+    } else this.showWindow();
 
-  private createWindow() {
-    this.mainWindow = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        preload: path.join(__dirname, "preload.js"), // Adjust the path to your preload script if needed
-      },
-    });
-
-    this.mainWindow.loadFile(path.join(__dirname, "index.html")); // Adjust the path to your HTML file if needed
-
-    this.mainWindow.on("closed", () => {
-      this.mainWindow = null;
+    app.on("ready", async () => {
+      console.info("🚀 Application Process Startup");
     });
   }
 
